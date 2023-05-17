983fbc96942fd6468557f3f35eb8bc5acd96e2b7
dianabam <dianabarrera.079@gmail.com>
2023-05-17 17:58:59 -0600

    Actividad 2

--- src/main/java/Poms/GooglePage.java
+++ src/main/java/Poms/GooglePage.java
@@ -1,5 +1,7 @@
 package Poms;//page object model, son clases que representan paginas
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,9 +11,13 @@ public class GooglePage {
 
 	WebDriver driver;
 	String searchBarId = "APjFqb";
-	String firstResult= "//h3[text()='Nintendo - Wikipedia']";
+	String firstResult= "//h3[text()='Wikipedia']";
 	String SecondResult= "//*[@id='firstHeading']/span";
-	
+	String resultClass = "//h3[@class='LC20lb MBeuO DKV0Md']";
+	String imprime1 = "//*[@id=\"mw-content-text\"]/div[1]/p[1]";
+	String imprime2 = "//*[@id=\"mw-content-text\"]/div[1]/p[3]";
+	String imprime3 = "//*[@id=\"mw-content-text\"]/div[1]/p[4]";
+
 	public GooglePage(WebDriver driver) {
 		super();
 		this.driver = driver;
@@ -28,7 +34,7 @@ public class GooglePage {
 		searchBar.sendKeys(Keys.ENTER);
 	}
 	public void clickFirstResult() {
-		WebElement fr = driver.findElement(By.xpath(firstResult));
+		WebElement fr = driver.findElement(By.xpath(firstResult));//buqueda por xpath
 		fr.click();
 	}
 	
@@ -36,5 +42,47 @@ public class GooglePage {
 		WebElement fr = driver.findElement(By.xpath(SecondResult));
 		System.out.println(fr.getText());
 				
+	}
+	
+	public void clickResultById(int result) {
+		
+		List <WebElement> list = driver.findElements(By.xpath(resultClass));
+		list.get(result).click();//obtiene el elemento result
+	}
+	
+	public void printTitle() {
+		System.out.println(driver.getTitle());
+	}
+	
+	public void printResults() {//imprime todos los resultados
+		List <WebElement> list = driver.findElements(By.xpath(resultClass));
+		
+		for(WebElement element: list) {
+			System.out.println(element.getText());
+		}
+	}
+	
+	public void print1() {//imprime todos los resultados
+		List <WebElement> list = driver.findElements(By.xpath(imprime1));
+		
+		for(WebElement element: list) {
+			System.out.println(element.getText());
+		}
+	}
+	
+	public void print2() {//imprime todos los resultados
+		List <WebElement> list = driver.findElements(By.xpath(imprime2));
+		
+		for(WebElement element: list) {
+			System.out.println(element.getText());
+		}
+	}
+	
+	public void print3() {//imprime todos los resultados
+		List <WebElement> list = driver.findElements(By.xpath(imprime3));
+		
+		for(WebElement element: list) {
+			System.out.println(element.getText());
+		}
 	}
 }
---
+++ src/main/java/Scripts/Sesion2.java
@@ -0,0 +1,55 @@
+package Scripts;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import Poms.GooglePage;
+
+public class Sesion2 {
+
+	
+	WebDriver driver; 
+	
+	@Before
+	public void setUp() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver(options);	
+		
+	
+	}
+		
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("https://www.google.com/");
+		GooglePage gp = new GooglePage(driver);
+		Thread.sleep(3000);
+		//busqueda google
+		gp.typeOnSearchBar("Wikipedia");
+		//click en primer resultado
+		//gp.clickFirstResult();// funcion
+		Thread.sleep(3000);
+		//gp.printFirstResult();//funcion
+		gp.clickResultById(1);
+		//gp.printTitle();
+		gp.print3();
+		gp.print2();
+		gp.print1();
+	}
+	
+	@After
+	public void teardown() {
+		
+		//driver.quit();
+	}
+
+	public Sesion2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
